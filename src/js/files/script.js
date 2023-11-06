b0623f470c0ede69d7c32f72725fa5fc0baf6c51
kilavki <kilavki@gmail.com>
2023-11-06 22:31:21 +0200

    add project home-services

---
+++ src/js/files/script.js
@@ -0,0 +1,66 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+const blogItems = document.querySelector('.blog__items');
+
+if (blogItems) {
+	loadBlogItems();
+}
+
+async function loadBlogItems() {
+	const response = await fetch("files/blog.json", {
+		method: "GET"
+	});
+	if (response.ok) {
+		const responseResult = await response.json();
+		initBlog(responseResult);
+	} else {
+		alert("Error!");
+	}
+}
+
+function initBlog(data) {
+	for (let index = 0; index < 3; index++) {
+		const item = data.items[index];
+		buildBlogItem(item);
+	}
+}
+
+function buildBlogItem(item) {
+	let blogItemTemplate = '';
+
+	blogItemTemplate += `<article class="blog__item item-blog">`;
+
+	item.image ? blogItemTemplate +=
+		`<a href="${item.url}" class="item-blog__image-ibg">
+			<img loading="lazy" src="${item.image}" alt="blog image">
+		</a>`
+		: null;
+
+	blogItemTemplate += `<div class="item-blog__date">${item.date}</div>`;
+
+	blogItemTemplate +=
+		`<h4 class="item-blog__title">
+			<a href="${item.url}" class="item-blog__link-title">${item.title}</a>
+		</h4>`;
+
+	item.text ? blogItemTemplate +=
+		`<div class="item-blog__text text">
+			${item.text}
+		</div>`
+		: null;
+	
+	if (item.tags) {
+		blogItemTemplate += `<div class="item-blog__tags">`;
+		for (const tag in item.tags) { 
+			blogItemTemplate += `<a href="${item.tags[tag]}" class="item-blog__tag">${tag}</a>`;
+		}
+		blogItemTemplate += `</div>`;
+	}
+	blogItemTemplate += `</article>`;
+
+	blogItems.insertAdjacentHTML('beforeend', blogItemTemplate);
+}
