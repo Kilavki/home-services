2dadf584f75b8604241b8fc93e8770a2073911e2
kilavki <kilavki@gmail.com>
2023-12-27 21:06:56 +0200

    add: behavior of button viewMore

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,65 +1,62 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
-
+import { flsModules } from './modules.js';
 
 const blogItems = document.querySelector('.blog__items');
-let counter = 3;
+let data;
+let startItem = 0;
+let endItem = 3;
 if (blogItems) {
 	loadBlogItems();
 }
 
 async function loadBlogItems() {
-	const response = await fetch("files/blog.json", {
-		method: "GET"
+	const response = await fetch('files/blog.json', {
+		method: 'GET',
 	});
 	if (response.ok) {
 		const responseResult = await response.json();
-		initBlog(responseResult);
+		data = responseResult;
+		initBlog(data, startItem, endItem);
 	} else {
-		alert("Error!");
+		alert('Error!');
 	}
 }
 
-function initBlog(data) {
-	for (let index = 0; index < counter; index++) {
-			const item = data.items[index];
-			console.log(item);
-			buildBlogItem(item);
-		}
-		// data.items.forEach((item, index) => {
-		// 	console.log(item.id);
-		// });
+function initBlog(data, startItem, endItem) {
+	const dataPart = data.items.slice(startItem, endItem);
+	dataPart.forEach(item => {
+		buildBlogItem(item);
+	});
+	viewMore();
 }
 
 function buildBlogItem(item) {
 	let blogItemTemplate = '';
+	blogItemTemplate += `<article data-id="${item.id}" class="blog__item item-blog">`;
 
-	blogItemTemplate += `<article class="blog__item item-blog">`;
-
-	item.image ? blogItemTemplate +=
-		`<a href="${item.url}" class="item-blog__image-ibg">
+	item.image
+		? (blogItemTemplate += `<a href="${item.url}" class="item-blog__image-ibg">
 			<img loading="lazy" src="${item.image}" alt="blog image">
-		</a>`
+		</a>`)
 		: null;
 
 	blogItemTemplate += `<div class="item-blog__date">${item.date}</div>`;
 
-	blogItemTemplate +=
-		`<h4 class="item-blog__title">
+	blogItemTemplate += `<h4 class="item-blog__title">
 			<a href="${item.url}" class="item-blog__link-title">${item.title}</a>
 		</h4>`;
 
-	item.text ? blogItemTemplate +=
-		`<div class="item-blog__text text">
+	item.text
+		? (blogItemTemplate += `<div class="item-blog__text text">
 			${item.text}
-		</div>`
+		</div>`)
 		: null;
-	
+
 	if (item.tags) {
 		blogItemTemplate += `<div class="item-blog__tags">`;
-		for (const tag in item.tags) { 
+		for (const tag in item.tags) {
 			blogItemTemplate += `<a href="${item.tags[tag]}" class="item-blog__tag">${tag}</a>`;
 		}
 		blogItemTemplate += `</div>`;
@@ -67,4 +64,24 @@ function buildBlogItem(item) {
 	blogItemTemplate += `</article>`;
 
 	blogItems.insertAdjacentHTML('beforeend', blogItemTemplate);
+}
+
+document.addEventListener('click', documentActions);
+
+function viewMore() {
+	const dataItemsLength = data.items.length;
+	const currentItems = document.querySelectorAll('.item-blog').length;
+	const viewMore = document.querySelector('.blog__view-more');
+	currentItems < dataItemsLength ? viewMore.hidden = false : viewMore.hidden = true;
+}
+
+function documentActions(e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.blog__view-more')) {
+		startItem = document.querySelectorAll('.item-blog').length;
+		endItem = startItem + 3;
+
+		initBlog(data, startItem, endItem);
+		e.preventDefault();
+	}
 }
